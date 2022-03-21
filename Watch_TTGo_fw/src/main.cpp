50a9f0c50a3d461d979d74778b66a8a756e37b1c
Cristian Sulighetean <cristian.sulighetean@cozify.fi>
2022-03-21 18:30:02 +0200

    Added test for gps

--- Watch_TTGo_fw/src/main.cpp
+++ Watch_TTGo_fw/src/main.cpp
@@ -1,9 +1,8 @@
 #include "config.h"
+#include "BluetoothSerial.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "BluetoothSerial.h"
 
 // Check if Bluetooth configs are enabled
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -13,20 +12,59 @@
 // Bluetooth Serial object
 BluetoothSerial SerialBT;
 
-volatile uint8_t state;
-volatile bool irqBMA = false;
-volatile bool irqButton = false;
-
+// Watch objects
 TTGOClass *watch;
 TFT_eSPI *tft;
 BMA *sensor;
+TinyGPSPlus *gps=nullptr;
+
+/************ Might not need ************/
+// 
+uint32_t last = 0;
+uint32_t updateTimeout = 0;
+
+/***************************************/
+
+
+volatile uint8_t state;
+volatile bool irqBMA = false;
+volatile bool irqButton = false;
 
 // Distance, step count 
 float distance = 0;
 uint32_t step_count = 0;
 
-void init_step_counter(void)
+void readFileToBL(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+        Serial.println("- failed to open file for reading");
+        return;
+    }
+
+    Serial.println("- read from file:");
+    while(file.available()){
+        //Serial.write(file.read());
+        SerialBT.println(file.read()); 
+    }
+    file.close();
+}
+
+void init_hike_watch()
 {
+    // LittleFS
+    if(!LITTLEFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
+        Serial.println("LITTLEFS Mount Failed");
+        return;
+    }
+
+    // GPS
+    watch->trunOnGPS();
+    watch->gps_begin();
+    gps = watch->gps;
+
+    // Stepcounter
     // Configure IMU
     Acfg cfg;
     cfg.odr = BMA4_OUTPUT_DATA_RATE_100HZ;
@@ -51,31 +89,34 @@ void init_step_counter(void)
 
     // Turn on step interrupt
     sensor->enableStepCountInterrupt();
+
+    // Side button
+    pinMode(AXP202_INT, INPUT_PULLUP);
+    attachInterrupt(AXP202_INT, [] {
+        irqButton = true;
+    }, FALLING);
+
+    //!Clear IRQ unprocessed first
+    watch->power->enableIRQ(AXP202_PEK_SHORTPRESS_IRQ, true);
+    watch->power->clearIRQ();
+
+    return;
 }
 
-void init_little_fs()
+void sendSession()
 {
-    if(!LITTLEFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
-        Serial.println("LITTLEFS Mount Failed");
-        return;
-    }
+    // Read session and send it via SerialBT
 }
 
-void readFileToBL(fs::FS &fs, const char * path){
-    Serial.printf("Reading file: %s\r\n", path);
+void checkSessionStored()
+{
+    // Check if session was stored
 
-    File file = fs.open(path);
-    if(!file || file.isDirectory()){
-        Serial.println("- failed to open file for reading");
-        return;
-    }
+}
 
-    Serial.println("- read from file:");
-    while(file.available()){
-        //Serial.write(file.read());
-        SerialBT.println(file.read()); 
-    }
-    file.close();
+void calculateDistance()
+{
+    // Calculate distance btw 2 points
 }
 
 void setup()
@@ -88,48 +129,60 @@ void setup()
     //Receive objects for easy writing
     tft = watch->tft;
     sensor = watch->bma;
-
-    watch->tft->fillScreen(TFT_BLACK);
-    watch->tft->drawString("Hiking Watch",  25, 50, 4);
-    watch->tft->setTextFont(4);
-    watch->tft->setTextColor(TFT_WHITE, TFT_BLACK);
     
-    // Initalize objects
-    init_step_counter();
-    init_little_fs();
+    init_hike_watch();
 
     state = 1;
 
-    pinMode(AXP202_INT, INPUT_PULLUP);
-    attachInterrupt(AXP202_INT, [] {
-        irqButton = true;
-    }, FALLING);
-
-    //!Clear IRQ unprocessed first
-    watch->power->enableIRQ(AXP202_PEK_SHORTPRESS_IRQ, true);
-    watch->power->clearIRQ();
-
-    SerialBT.begin("ESP32");
+    SerialBT.begin("Hiking Watch");
 }
 
 
 void loop()
 {
+    watch->gpsHandler();
 
     switch (state)
     {
     case 1:
     {
-        /* Stand-by mode*/
-        watch->tft->fillRect(0, 0, 240, 240, TFT_BLACK);
-        watch->tft->drawString("Start button", 25, 100);
-        watch->tft->drawString("to start session", 10, 125);
-        /* Initialisations */
+        /* Initial stage */
+        watch->tft->fillScreen(TFT_BLACK);
+        watch->tft->setTextFont(4);
+        watch->tft->setTextColor(TFT_WHITE, TFT_BLACK);
+        watch->tft->drawString("Hiking Watch",  45, 25, 4);
+        watch->tft->drawString("Press button", 50, 80);
+        watch->tft->drawString("to start session", 40, 110);
+
+        bool sessionStored = false;
+        bool sessionSent = false;
+        // Check if we have a stored session
+
+        // If we have a session
+        // Print on the watch that we sync and download session to hub
+        
+        Serial.println(state);
 
         while (1)
         {
             /* code */
-
+            /* Bluetooth sync function */
+            if (SerialBT.available())
+            {
+                char incomingChar = SerialBT.read();
+                if (incomingChar == 'c' and sessionStored and not sessionSent){
+                    // Send session via bluetooth
+                    sendSession();
+                    // Send connection termination char
+                    SerialBT.write('c');
+                    sessionSent = true;
+                }
+                // TODO verify if receive char is r
+                if (sessionSent && incomingChar == 'r')
+                {
+                    Serial.println("Session successfuly send via BT");
+                }
+            }
 
             /*      IRQ     */
             if (irqButton) {
@@ -150,17 +203,14 @@ void loop()
     case 2:
     {
         /* Hiking session initalisation */
+        Serial.println(state);
 
         // Clear screen
         watch->tft->fillRect(0, 0, 240, 240, TFT_BLACK);
         watch->tft->drawString("Initializing watch", 25, 100);
 
         // Do the initializations
-        // Dummy delay
-        delay(1000);
-
-        // Reset steps count
-        sensor->resetStepCounter(); 
+        delay(1000); 
 
         distance = 0;
         step_count = 0;
@@ -175,16 +225,80 @@ void loop()
     case 3:
     {
         /* Hiking session ongoing */
+        Serial.println(state);
+
+        // Reset steps count
+        sensor->resetStepCounter();
 
         // Setup
         watch->tft->fillRect(0, 0, 240, 240, TFT_BLACK);
-        watch->tft->drawString("Starting hike", 25, 100);
+        watch->tft->drawString("Starting hike", 45, 100);
         delay(1000);
         watch->tft->fillRect(0, 0, 240, 240, TFT_BLACK);
+        
+        watch->tft->setCursor(45, 40);
+        watch->tft->print("Step Int: 0");
+
+        watch->tft->setCursor(45, 70);
+        watch->tft->print("Step API: 0");
+
+        watch->tft->setCursor(45, 100);
+        watch->tft->print("Distance: 0 km");
+
+        last = millis();
+
+        // Lat long
+        float lat = 0;
+        float lng = 0;
+        float past_lat = 0;
+        float past_lng = 0;
+        bool coord_printed = false;
 
         while (1)
         {   
-            /* A non-blocking delay. */
+            /* GPS */
+            // Get position
+            if (gps->location.isUpdated()) {
+                updateTimeout = millis();
+                lat = gps->location.lat();
+                lng = gps->location.lng();
+
+                watch->tft->setCursor(45, 130);
+                watch->tft->print("Valid GPS");
+                watch->tft->setCursor(45, 160);
+                watch->tft->print(lat);
+                watch->tft->setCursor(45, 190);
+                watch->tft->print(lng);
+
+            } else {
+                if (millis() - updateTimeout > 3000) {
+                    updateTimeout = millis();
+
+                    Serial.println("Invalid location");
+
+                    watch->tft->setCursor(45, 130);
+                    watch->tft->print("Invalid GPS");
+
+                }
+            }
+
+            if (lat != past_lat && lng != past_lng)
+            {
+                Serial.print("LAT: ");Serial.print(lat);
+                Serial.print(" LNG: ");Serial.print(lng);
+                Serial.println();
+
+                past_lat = lat;
+                past_lng = lng;
+            }
+
+            // If the coord are the same drop them 
+            // else save to coord.txt
+
+            // Calculate distance
+
+            // Add to current distance and display it to user
+
 
             /* Step counter */
             if (irqBMA) {
@@ -205,19 +319,15 @@ void loop()
 
                     // Get step data from register
                     watch->tft->setTextColor(TFT_WHITE, TFT_BLACK);
-                    watch->tft->setCursor(45, 118);
-                    watch->tft->print("StepCount:");
+                    watch->tft->setCursor(45, 40);
+                    watch->tft->print("Step Int: ");
                     watch->tft->print(step_count);
+
+                    watch->tft->setCursor(45, 70);
+                    watch->tft->print("Step API: ");
+                    watch->tft->print(sensor->getCounter());
                 }
             }
-
-            /* GPS */
-
-            /* Distance calculation */
-
-            /* Saving to file */
-
-            delay(20);
 
             /*      IRQ     */
             if (irqButton) {
@@ -232,35 +342,29 @@ void loop()
             if (state == 4) {
                 break;
             }
+                
         }
-
-        // Step counter code
-        // Save step count
-        
-        // Get gps location
-        // Save gps location
-
-        // Calculate current distance
-        
         break;
     }
     case 4:
     {
-        // Saving step, distance, coordinates 
-        // Close file
+        // Saving id, steps and distance in different files 
         watch->tft->fillRect(0, 0, 240, 240, TFT_BLACK);
-        watch->tft->drawString("Save state", 25, 100);
-
-        // Save step counter to file
-        listDir(LITTLEFS, "/", 3);
+        watch->tft->drawString("Saving hike", 25, 100);
         
         char buffer[30];
         itoa(step_count, buffer, 10);
         Serial.print("Buffer: "); Serial.println(buffer);
         writeFile(LITTLEFS, "/steps.txt", buffer);
         
+        Serial.println(state);
+
         while (1)
         {
+            if (SerialBT.available()){
+                char incomingChar = SerialBT.read();
+                Serial.println(incomingChar);
+            }    
             /*      IRQ     */
             if (irqButton) {
                 irqButton = false;
@@ -281,17 +385,14 @@ void loop()
 
     case 5:
     {
-        /* Sending session via bluetooth */
         watch->tft->fillRect(0, 0, 240, 240, TFT_BLACK);
-        watch->tft->drawString("Sync", 25, 100);
+        watch->tft->drawString("State 5", 25, 100);
 
-        readFileToBL(LITTLEFS, "/steps.txt");
-        SerialBT.println(step_count); 
-        
+        Serial.println(state);
+
 
         while (1)
         {
-
             /*      IRQ     */
             if (irqButton) {
                 irqButton = false;
@@ -311,7 +412,11 @@ void loop()
     }
     case 6:
     {
-        /* Bluetooth sync */
+        watch->tft->fillRect(0, 0, 240, 240, TFT_BLACK);
+        watch->tft->drawString("State 6", 25, 100);
+
+        Serial.println(state);
+
         while (1)
         {
             // After finish sync or IRQ, go to fist state
