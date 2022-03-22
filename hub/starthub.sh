d3062e59c9eaa2e76db9495651a9678cf2390c58
Gabor Pelesz <gaborpelesz@gmail.com>
2022-03-22 22:47:24 +0100

    something not as sudo

--- hub/starthub.sh
+++ hub/starthub.sh
@@ -1,7 +1,7 @@
-python3 wserver.py &
+sudo -u hikingHub python3 wserver.py &
 PID_SERVER=$!
 sudo python3 led.py &
 PID_LED=$!
-python3 receiver.py &
+sudo -u hikingHub python3 receiver.py &
 PID_RECEIVER=$!
 echo "$PID_SERVER"$'\n'"$PID_LED"$'\n'"$PID_RECEIVER" > .bg_processes
