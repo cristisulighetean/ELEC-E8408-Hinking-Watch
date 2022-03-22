b6a61a1aa71fe5f6bd31ff98d738b13fdc6899e0
Gabor Pelesz <gaborpelesz@gmail.com>
2022-03-22 22:59:51 +0100

    try disown

--- hub/starthub.sh
+++ hub/starthub.sh
@@ -1,7 +1,7 @@
-sudo -u hikingHub python3 wserver.py &
+sudo -u hikingHub python3 wserver.py & disown
 PID_SERVER=$!
-sudo python3 led.py &
+sudo python3 led.py & disown
 PID_LED=$!
-sudo -u hikingHub python3 receiver.py &
+sudo -u hikingHub python3 receiver.py & disown
 PID_RECEIVER=$!
 echo "$PID_SERVER"$'\n'"$PID_LED"$'\n'"$PID_RECEIVER" > .bg_processes
