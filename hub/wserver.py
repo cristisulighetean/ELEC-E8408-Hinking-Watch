a6d84b6823f989a1072de66bd42959a893ad2441
Gabor Pelesz <gaborpelesz@gmail.com>
2022-03-22 17:45:10 +0100

    fix wserver

--- hub/wserver.py
+++ hub/wserver.py
@@ -3,22 +3,32 @@ from flask import render_template
 from flask import jsonify
 
 import db
+import hike
 
 app = Flask(__name__)
+hdb = db.HubDatabase()
 
 @app.route('/')
 def url():
-    return render_template('home.html')
+    sessions = hdb.get_sessions() 
+    return render_template('home.html', sessions=sessions)
 
-# @app.route('/sessions')
-# def search_options():
-#     timestamp_list = db.get_timestamps() 
-#     return jsonify(timestamp_list)
+@app.route('/sessions')
+def search_options():
+    sessions = hdb.get_sessions() 
+    sessions = list(map(lambda s: hike.to_list(s), sessions))
+    print(sessions)
+    return jsonify(sessions)
 
-# @app.route('/sessions/<id>')
-# def search_parameters(id):
-#     parameters_dict = db.get_session_data(id)
-#     return jsonify(list(parameters_dict.values()))
+@app.route('/sessions/<id>')
+def search_parameters(id):
+    session = hdb.get_session(id)
+    return jsonify(hike.to_list(session))
+
+@app.route('/sessions/<id>/delete')
+def delete_session(id):
+    hdb.delete(id)
+    print(f'DELETED SESSION WITH ID: {id}')
 
 if __name__ == "__main__":
     app.run(debug=True)
